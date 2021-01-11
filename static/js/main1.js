368eef45f992601ab414661cd44adfc7d1fd3d3e
lvaughen <69725356+lvaughen@users.noreply.github.com>
2021-01-10 19:28:00 -0500

    testing parameters

--- static/js/main1.js
+++ static/js/main1.js
@@ -1,7 +1,7 @@
 function loadMsrank() {
     console.log("fix1")
     d3.json("/api/ms_rank").then((data) => {
-        console.log("fix2");
+        console.log(data);
         
         // data.forEach((row)) => {
         //     var listGroup = d3.select("#row");
